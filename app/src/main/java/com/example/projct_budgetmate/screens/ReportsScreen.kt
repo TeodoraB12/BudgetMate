6bb5658e5f1b546bba7a4340b584f61192415f0a
Teodora <bojadzieskat@gmail.com>
2025-06-01 22:42:42 +0200

    Translation on application

--- app/src/main/java/com/example/projct_budgetmate/screens/ReportsScreen.kt
+++ app/src/main/java/com/example/projct_budgetmate/screens/ReportsScreen.kt
@@ -13,8 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.example.projct_budgetmate.data.db.BudgetTransaction
 import com.example.projct_budgetmate.data.db.Category
-import com.example.projct_budgetmate.model.Transaction
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
@@ -22,7 +22,7 @@ import com.github.mikephil.charting.data.PieEntry
 
 @Composable
 fun ReportsScreen(
-    transactions: List<Transaction>,
+    transactions: List<BudgetTransaction>,
     categories: List<Category>
 ) {
     Column(modifier = Modifier
